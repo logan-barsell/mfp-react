b1f40d94cd0aad5603a382e787ad3dca18183e22
Logan Barsell <loganjbars@gmail.com>
2021-11-06 17:45:58 -0700

    container.yml-invalidation-last time hopefully

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
     return (
         <div>
-            <h1>Hello!!!!</h1>
+            <h1>Hello WORLD!!!!</h1>
             <hr />
             <MarketingApp />
         </div>
