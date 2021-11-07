4756a87b65081590186a31b853e75ad2afdd58ea
Logan Barsell <loganjbars@gmail.com>
2021-11-07 14:20:48 -0800

    added auth subapp

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -4,18 +4,17 @@ import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 //Mount function to start up the app
-const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
     //if app is being rendered in isolation it uses default history, otherwise it uses memory history
     const history = defaultHistory || createMemoryHistory({
         initialEntries: [initialPath],
     });
-
     if (onNavigate){
 // Whenever the url/path changes, call onNavigate function
         history.listen(onNavigate);
     }
 
-    ReactDOM.render(<App history={history} />, el);
+    ReactDOM.render(<App onSignIn={onSignIn} history={history} />, el);
 
     return {
         onParentNavigate({ pathname: nextPathname }) {
@@ -30,7 +29,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 // If we are in development and in isolation,
 // call mount immediately
 if ( process.env.NODE_ENV === 'development') {
-    const devRoot = document.querySelector('#_marketing-dev-root');
+    const devRoot = document.querySelector('#_auth-dev-root');
 
     if (devRoot) {
         mount(devRoot, { defaultHistory: createBrowserHistory() });
